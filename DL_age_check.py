24b836ddd9ea2758780b430c9931eaeee4c984fd
TLeonidas <124396089+TLeonidas@users.noreply.github.com>
2023-02-03 22:18:00 -0600

    Add files via upload
    Uploaded file from local

---
+++ DL_age_check.py
@@ -0,0 +1,12 @@
+# Drive License age check
+print('-----------------' + 'DEPARTAMENTO DE TRANSITO' + '-----------------')
+print('- - - - - - - -  DRIVE LICENSE AGE CHECK  - - - - - - - -')
+
+ano_atual = int(2023)
+ano_nasc = int(input('Digite o ano do seu nascimento: '))
+quanto_falta = int(18 - (ano_atual - ano_nasc))
+
+if (ano_atual - ano_nasc) >= 18:
+  print('Voce ja possui maioridade')
+else:
+  print('Voce ainda precisa aguardar ' + str(quanto_falta) + ' anos para poder tirar a carteira.')
